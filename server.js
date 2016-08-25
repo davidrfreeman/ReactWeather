d369bd4484ade6ed5674de4338bb203e90575a2f
David Freeman <davidfreeman@holos.solutions>
2016-08-24 22:20:49 -0600

    prep repo for heroku

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  return (req.headers['x-forwarded-proto'] === 'http') ? next() : res.redirect('http://' + req.hostname + req.url);
+});
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, res) {
